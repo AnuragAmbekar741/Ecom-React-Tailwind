9a2c1138b87dcfe935df5462379c4c856cefe1c3
AnuragAmbekar741 <anuragambekar1997@gmail.com>
2023-10-05 01:33:53 +0530

    pov - video

--- components/Home.tsx
+++ components/Home.tsx
@@ -1,16 +1,32 @@
+"use client"
+import React from "react";
 import Image from 'next/image'
-import React from 'react'
+
+
+
+const src =
+  "https://res.cloudinary.com/dmprrczfn/video/upload/v1696447239/vdcsi03nc5zymbnlc2kw.mp4";
+
+const Video = () => {
+  return (
+    <video loop autoPlay controls={false} muted width="100%">
+      <source src={src} type="video/mp4" />
+      Sorry, your browser doesn't support embedded videos.
+    </video>
+  );
+};
 
 const Pov:React.FC = () => {
   return (
     <section>
-        <Image
+      <Video/>
+        {/* <Image
             src="/assets/pov.png"
             alt='pov'
             width={1500}
             height={1500}
             className='w-screen h-screen'
-        /> 
+        />  */}
     </section>
   )
 }
