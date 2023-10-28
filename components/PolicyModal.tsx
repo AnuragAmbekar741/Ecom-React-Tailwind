ba187a9df4976ae5f6f758f4cbad1a6b483db730
Anurag <anuragambekar1997@gmail.com>
2023-10-29 02:29:45 +0530

    footer & first fold final reponsive changes -2

--- components/PolicyModal.tsx
+++ components/PolicyModal.tsx
@@ -16,10 +16,12 @@ const PolicyModal = (props: Props) => {
   return (
     <div className={`fixed top-0 w-full h-full bg-[#00000099] ${props.hide?'block':'hidden'} `}>
     <div className="grid mx-auto top-20 right-3 left-3 md:top-60 md:w-1/2 p-7 absolute bg-white text-black">
+      <div onClick={()=>props.setHide(false)}>
         <RxCross2 
             className='text-xl absolute right-3 top-3'
             onClick={()=>props.setHide(false)}
         />
+      </div>
         <h3 className='text-2xl font-light text-left my-2'>{props.policyName}</h3>
         <p className='text-md font-light text-left mb-2'>{props.policy}</p>
     </div>
