deae47de5bcfd9ecd93ef5cb87185601d5d9b37c
Anurag <anuragambekar1997@gmail.com>
2023-12-17 16:56:32 +0530

    About changes

--- components/About.tsx
+++ components/About.tsx
@@ -12,7 +12,7 @@ const About = () => {
             <div className='w-full lg:w-4/6'>
             <div className='w-full justify-center p-8 lg:px-4 lg:py-1 text-left'>
                 <h1 className='text-[27px] md:text-[38px] md:my-2 tracking-widest font-light'>ABOUT THE BRAND</h1>
-                <h3 className='text-[16.99px] md:text-[19px] sm:my-3 font-light leading-8'>
+                <h3 className='text-[16.99px] md:text-[19px] sm:my-3 font-light leading-7'>
                    <span>RheaVania is a newly launched luxury clothing label that believes in dressing for your body. </span><br/> 
                    <span>Our designs are made for all body types, ensuring that no matter what shape or size, you're going to look stunning!</span><br/>
                    <span>At RheaVania our emphasis lies in elevating female silhouette. </span><br/> 
@@ -22,7 +22,7 @@ const About = () => {
                    <span>Our designs are inspired by some of the most fierce, leading and authoritative women and their stories across the globe. We hope to inspire a few more in the process.</span>
                 </h3>
             </div>
-            <div className='w-full justify-end px-8 lg:px-4 lg:pb-5 text-left'>
+            <div className='w-full justify-end px-8 lg:px-4 lg:pb-5 text-left leading-7'>
                 <h1 className='text-[27px] md:text-[38px] md:mb-1 tracking-wider font-light mt-3'>ABOUT THE DESIGNER</h1>
                 <h3 className='text-[16.99px] md:text-[19px] font-light'>The creator of this passion project is Rhea Vania De Quadros. She hails from Goa, India. Currently based in Mumbai, Rhea has worked for various brands, celebrities and assisted the top stylist in the industry!</h3>
             </div>
